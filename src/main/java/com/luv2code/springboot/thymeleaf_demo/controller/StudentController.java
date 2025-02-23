6fbbc31550b1921d15ffc071f9bc0391e5c12ceb
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-02-23 16:55:07 -0600

    add Student model

--- src/main/java/com/luv2code/springboot/thymeleaf_demo/controller/StudentController.java
+++ src/main/java/com/luv2code/springboot/thymeleaf_demo/controller/StudentController.java
@@ -1,18 +1,41 @@
 package com.luv2code.springboot.thymeleaf_demo.controller;
 
+import com.luv2code.springboot.thymeleaf_demo.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StudentController {
 
-    @RequestMapping("/student-form")
-    public String getStudentForm() {
+    @GetMapping("/studentForm")
+    public String getStudentForm(Model model) {
+        model.addAttribute("student", new Student());
         return "student-form";
     }
 
-    @RequestMapping("/process-form")
+    @PostMapping("/processForm")
     public String processStudentForm() {
         return "process-form";
+    }
+
+    @PostMapping("/processFormV2")
+    public String processFormV2(HttpServletRequest request, Model model) {
+        String name = request.getParameter("studentName");
+        name = name.toUpperCase();
+        model.addAttribute("message", name);
+        return "process-form-v2";
+    }
+
+    @PostMapping("/processFormV3")
+    public String processFormV3(@RequestParam("studentName") String studentName, Model model) {
+
+        studentName = "From v3: " + studentName;
+        model.addAttribute("message", studentName);
+        return "process-form-v3";
     }
 }
